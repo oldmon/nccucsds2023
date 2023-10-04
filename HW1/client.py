ab6d9a3f371d7dc014cccfb29f7714296139454f
Nicholas Ou <oldmonou@gmail.com>
2023-10-05 01:10:57 +0800

    the finished version

--- HW1/client.py
+++ HW1/client.py
@@ -1,27 +1,10 @@
 #！/usr/bin/env python3
 import socket
-
-# 建立 UDP socket
-sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-
-# 連接到本地端的 UDP Server，port 為 20213
-server_address = ("127.0.0.1", 20213)
-sock.connect(server_address)
-
-# 將訊息轉為 binary
-message = b"This is a test from python client"
-
-# 送出訊息
-sock.sendall(message)
-
-# 接收回應訊息
-data, address = sock.recvfrom(1024)
-
-# 將 binary 轉回文字
-message = data.decode("utf-8")
-
-# 印出回應訊息
-print(message)
-
-# 關閉連線
-sock.close()
+s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+s.connect(("127.0.0.1", 20213))
+msg = b"This is a test from python client"
+s.send(msg)
+data, address = s.recvfrom(1024)
+rmsg = data.decode("utf-8")
+print(rmsg)
+s.close()
--- HW1/server.js
+++ HW1/server.js
@@ -1,21 +1,17 @@
-const net = require("net");
+const dgram = require('dgram');
 
-const server = net.createServer((socket) => {
-  // 當收到訊息時
-  socket.on("message", (data) => {
-    // 在 console 中印出所接收到的訊息
-    console.log(data);
-
-    // 取得 Client 的 port number
-    const port = socket.remotePort;
-
-    // 將收到的訊息的最前面加上「XXXX:」再送回給 Client
-    socket.send(`XXXX:${data}`, () => {
-      // 立即關閉連線
-      socket.close();
-    });
+const server = dgram.createSocket('udp4');
+server.on('message', (msg, rinfo) => {
+  console.log(msg);
+  const port = rinfo.port;
+  server.send(`${port}:${msg}`,rinfo.port,rinfo.address, () => {
+    server.close();
   });
 });
 
-// 傾聽本地端的 port 20213
-server.listen(20213);
+server.on('error', (err) => {
+  console.log(err);
+});
+
+// Listen port on 20213
+server.bind(20213);
